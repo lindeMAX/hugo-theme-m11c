01bb6311896b0a8c81e9da5d885c2be8747a8180
lindeMAX <max.lindemann@posteo.net>
2022-08-01 13:16:59 +0200

    Don't remember

--- static/base.js
+++ static/base.js
@@ -1,16 +1,28 @@
 window.onload = function () {
-    
+    // Try to read from local storage, otherwise set to default
+    let currentTheme = localStorage.getItem("mytheme") || "dark-theme"
+
+    body.classList.add(currentTheme);
+
+    const themeToggle = document.getElementById('themeToggle');
+
+    themeToggle.onclick = function () {
+        this.classList.toggle("checked");
+        if (themeToggle.checked) {
+            body.classList.remove('light-theme');
+            body.classList.add('dark-theme');
+            localStorage.setItem("mytheme", 'dark-theme');
+        }
+        else {
+            body.classList.remove('dark-theme');
+            body.classList.add('light-theme');
+            localStorage.setItem("mytheme", 'light-theme');
+        }
+    };
+
     const hamburger = document.getElementById("hamburger");
     const fadeshow = document.getElementById("fadeshow");
 
-    // Display header if is checked -> This is so on the home page
-    if (hamburger.checked) {
-        fadeshow.style.display = "block";
-    }
-    else {
-        fadeshow.style.display = "none";
-    }
-    
     hamburger.onclick = function () {
         this.classList.toggle("checked");
         if (hamburger.checked) {
@@ -21,34 +33,7 @@ window.onload = function () {
         }
     }
 
-    var items = document.body.getElementsByTagName("*");
-    for (var i = 0, len = items.length; i < len; i++) {
-        // do something with items[i] here
-        console.log(items[i]);
+    if ($(window).width() < "960") {
+        hamburger.onclick();
     }
-
-    // Apply color theme
-    function setTheme(oldTheme, newTheme) {
-        const body = document.getElementsByTagName("body");
-        // Remove old theme scope from body's class list
-        body.classList.remove(oldTheme);
-        // Add new theme scope to body's class list
-        body.classList.add(newTheme);
-        // Set it as current theme
-        currentTheme = newTheme;
-    }
-
-    const themeSelector = document.getElementById("theme-selector");
-    
-    // Set to default theme on load
-    let currentTheme = "dark-theme";
-    body.classList.add(currentTheme);
-
-    // Add change event listener
-    themeSelector.addEventListener("change", function (e) {
-        // Get the user's choice from the event object `e`.
-        const newTheme = e.currentTarget.value;
-        // Set the theme
-        setTheme(currentTheme, newTheme);
-    });
 };
